8d80ab9b935bbce9ee8623ea723c60bf4ebbd6a0
Frederik-Vagner <70664117+Frederik-Vagner@users.noreply.github.com>
2021-10-11 20:04:25 -0400

    Added java classes

---
+++ src/main/java/ca/gbc/comp3095/gbccomp3095petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package ca.gbc.comp3095.gbccomp3095petclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/ca/gbc/comp3095/gbccomp3095petclinic/model/Pet.java
@@ -0,0 +1,11 @@
+package ca.gbc.comp3095.gbccomp3095petclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthdate;
+
+}
