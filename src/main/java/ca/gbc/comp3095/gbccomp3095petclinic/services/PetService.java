6ae88e8a2f203ebd53b8205c5cf7a98de5d5f971
Frederik-Vagner <70664117+Frederik-Vagner@users.noreply.github.com>
2021-10-11 20:32:40 -0400

    Added java classes

---
+++ src/main/java/ca/gbc/comp3095/gbccomp3095petclinic/services/PetService.java
@@ -0,0 +1,12 @@
+package ca.gbc.comp3095.gbccomp3095petclinic.services;
+
+import ca.gbc.comp3095.gbccomp3095petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
